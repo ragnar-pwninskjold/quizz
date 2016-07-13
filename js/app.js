bd8d813ec979de587cb2406cb22c1d4bd274c324
ragnar-pwninskjold <seankochel@gmail.com>
2016-07-13 08:20:47 -0400

    step 1 refactor - updated the character and question arrays

--- js/app.js
+++ js/app.js
@@ -4,53 +4,35 @@ $(document).ready(function() {
 	//--> on "take it again" click, reset all values in game to 0
 
 //declare the question object
-var questionformat = {
-	question: "Question 1 text",
-	a1: "Answer 1 text",
-	a2: "Answer 2 text",
-	a3: "Answer 3 text",
-	a4: "Answer 4 text",
-	a5: "Answer 5 text",
-	key: function(n) {
-		return this[Object.keys(this)[n]];
-	}
-};
+
 //make each question-answer pair
-var question1 = Object.create(questionformat);
-var question2 = Object.create(questionformat);
-var question3 = Object.create(questionformat);
-var question4 = Object.create(questionformat);
-var question5 = Object.create(questionformat);
-question1.question = "Which line best describes you?";
-question1.a1 = "I'm magical";
-question1.a2 = "Thoroughly annoying to my 'friends' ";
-question1.a3 = "Dark and broody";
-question1.a4 = "Some say you're 'special' ";
-question1.a5 = "I'm fabulous";
-question2.question = "Which climate best suits you?";
-question2.a1 = "Temperate";
-question2.a2 = "Too humid";
-question2.a3 = "Extreme heat";
-question2.a4 = "What's a climate?";
-question2.a5 = "The tropics";
-question3.question = "If you could have one power, what would it be?";
-question3.a1 = "Magic";
-question3.a2 = "Breathing underwater";
-question3.a3 = "Telekenesis";
-question3.a4 = "Ka-ra-te!";
-question3.a5 = "A mic of steel";
-question4.question = "Signature dance move?";
-question4.a1 = "I don't dance";
-question4.a2 = "'The Flail'";
-question4.a3 = "'The Robot'";
-question4.a4 = "The 'Nae Nae'";
-question4.a5 = "'Pass the Dishes'";
-question5.question = "What weapon would you bring to a street fight?";
-question5.a1 = "An invisibility cloack and a sharp blade";
-question5.a2 = "A powerful electrical... stick";
-question5.a3 = "Can I use telekenesis twice?";
-question5.a4 = "Brass Knuckles";
-question5.a5 = "A highnote that could kill a man";
+var questionArray = [
+	{
+		question: "Which line best describes you?", 
+		answers: ["I'm magical", "Thoroughly annoying to my 'friends' ","Dark and broody", "Some say you're 'special' ", "I'm fabulous"]
+	},
+
+	{
+		question: "Which climate best suits you?", 
+		answers: ["Temperate", "Too humid","Extreme heat" , "What's a climate?","The tropics"]
+	},
+
+	{
+		question: "If you could have one power, what would it be?" , 
+		answers: ["Magic", "Breathing underwater", "Telekenesis", "Ka-ra-te!", "A mic of steel"]
+	},
+
+	{
+		question: "Signature dance move?" , 
+		answers: ["I don't dance", "'The Flail'","'The Robot'","The 'Nae Nae'", "'Pass the Dishes'"  ]
+	},
+
+	{
+		question: "What weapon would you bring to a street fight?" , 
+		answers: ["An invisibility cloack and a sharp blade", "A powerful electrical... stick", "Can I use telekenesis twice?", "Brass Knuckles", "A highnote that could kill a man" ]
+	}
+];
+
 
 //make object for characters
 var characters = {
@@ -64,41 +46,45 @@ var characters = {
 };
 
 //declare character types that people can get matched with
-var ronWeasley = Object.create(characters);
-ronWeasley.character = "Ron Weasley";
-ronWeasley.movie = "Harry Potter";
-ronWeasley.description = "Congratulations? Looks like you landed yourself in the wizardly world of Harry Potter and friends. Your best friend is the most powerful wizard in the world, but it seemingly eats you up inside. Quidditch? Potions class? Being the chosen one? Destructive magic? Battling dark lords? Harry is beating you at every turn you take. You need to step up your game Ronald Weasley.";
-ronWeasley.link = "http://images.hellogiggles.com/uploads/2015/07/10/Ron-Weasley-e1436589589658.jpg";
-
-var jjBinks = Object.create(characters);
-jjBinks.character = "Jar-jar Binks";
-jjBinks.movie = "Star Wars, Episode II";
-jjBinks.description = "'Ay-yee-yee! Wha! Was’n dat. Hey, wait! Oh, mooie-mooie! I love you!' Wow. Looks like you got Jar-jar binks. You single handedly served to ruin an entire movie. You-sa should take this quiz again-sa. 'OOOOOEEEYOOOOOEEEEEEEE'";
-jjBinks.link = "http://www.themarysue.com/wp-content/uploads/2015/05/16-dallas-cowboys-jar-jar-binks_pg_600-640x476.jpg";
-
-var aWalker = Object.create(characters);
-aWalker.character = "Anakin Skywalker";
-aWalker.movie = "Star Wars, Episode II";
-aWalker.description = "Is she safe? Is she all right? Well, now you've done it Lord Vader. You made a deal with the devil and lost the love of your life. It looks li - wha - what are you doi - *narrator is force choked* NOOO *choking sounds*";
-aWalker.link = "http://im.ziffdavisinternational.com/t/ign_in/news/a/anakin-sky/anakin-skywalker-could-have-been-in-star-wars-the_zt55.640.jpg";
-
-var mattDamon = Object.create(characters);
-mattDamon.character = "Matt Damon";
-mattDamon.movie = "Matt Damon";
-mattDamon.description = "MATT DAAAAMON";
-mattDamon.link = "http://www.interfaithstrength.com/Newt2_files/Matt-Damon-Puppet.jpg";
-
-var rRhod = Object.create(characters);
-rRhod.character = "Ruby Rhod";
-rRhod.movie = "5th Element";
-rRhod.description = "In 'The Fifth Element', Ruby Rhod is a famous host of a radio show with a quarter of The Federated Territories population (over fifty billion people) listening to his show. He is an incredibly annoying character. These quiz results do not bode well for you. I recommend retaking this quiz";
-rRhod.link = "http://vignette4.wikia.nocookie.net/bloodandhonor/images/7/74/Ruby_Rhod.jpg/revision/latest?cb=20120709161623";
 
 //put all characters in an array
-var characterArray = [ronWeasley, jjBinks, aWalker, mattDamon, rRhod];
+var characterArray = [
+
+{
+	character: "Ron Weasley",
+	movie: "Harry Potter",
+	description: "Congratulations? Looks like you landed yourself in the wizardly world of Harry Potter and friends. Your best friend is the most powerful wizard in the world, but it seemingly eats you up inside. Quidditch? Potions class? Being the chosen one? Destructive magic? Battling dark lords? Harry is beating you at every turn you take. You need to step up your game Ronald Weasley.",
+	link: "http://images.hellogiggles.com/uploads/2015/07/10/Ron-Weasley-e1436589589658.jpg"
+},
+{
+	character: "Jar-jar Binks",
+	movie: "Star Wars, Episode II",
+	description: "'Ay-yee-yee! Wha! Was’n dat. Hey, wait! Oh, mooie-mooie! I love you!' Wow. Looks like you got Jar-jar binks. You single handedly served to ruin an entire movie. You-sa should take this quiz again-sa. 'OOOOOEEEYOOOOOEEEEEEEE'",
+	link: "http://www.themarysue.com/wp-content/uploads/2015/05/16-dallas-cowboys-jar-jar-binks_pg_600-640x476.jpg"
+},
+{
+	character: "Anakin Skywalker",
+	movie: "Star Wars, Episode II",
+	description: "Is she safe? Is she all right? Well, now you've done it Lord Vader. You made a deal with the devil and lost the love of your life. It looks li - wha - what are you doi - *narrator is force choked* NOOO *choking sounds*",
+	link: "http://im.ziffdavisinternational.com/t/ign_in/news/a/anakin-sky/anakin-skywalker-could-have-been-in-star-wars-the_zt55.640.jpg"
+},
+{
+	character: "Matt Damon",
+	movie: "Matt Damon",
+	description: "MAAAAT DDAAAAAMOONNN",
+	link: "http://www.interfaithstrength.com/Newt2_files/Matt-Damon-Puppet.jpg"
+},
+{
+	character: "Ruby Rhod",
+	movie: "The Fifth Element",
+	description: "In 'The Fifth Element', Ruby Rhod is a famous host of a radio show with a quarter of The Federated Territories population (over fifty billion people) listening to his show. He is an incredibly annoying character. These quiz results do not bode well for you. I recommend retaking this quiz",
+	link: "http://vignette4.wikia.nocookie.net/bloodandhonor/images/7/74/Ruby_Rhod.jpg/revision/latest?cb=20120709161623"
+}
+
+];
 
 //put questions into an array
-var questionArray = [question1, question2, question3, question4, question5];
+
 var quizPosition = 0;
 
 //populate a question on the screen using object for questions and answers
@@ -171,7 +157,7 @@ function generateQuestion (x, qArray) {
 if (x < 5) {
 var position = qArray[x];
 var question = position.question;
-var answers = [position.a1, position.a2, position.a3, position.a4, position.a5];
+var answers = position.answers;
 var liCount = 0;
 $(".thequestion").text(question);
 $("label").each(function() {
